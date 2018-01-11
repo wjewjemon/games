30c0dcc53b5ad2cbb49dbd77c346d822afc599ac
Ben Cook <bencook400@gmail.com>
2018-01-11 03:05:58 -0500

    first commit

---
+++ snake/app.js
@@ -0,0 +1,112 @@
+// VARIABLES
+var canvas, ctx;
+canvas = document.getElementById("snakeCanvas");
+ctx = canvas.getContext("2d");
+
+// game variables
+var score, lives;
+
+// grid variables
+var rows, columns, sideLength;
+sideLength = 10;
+rows = canvas.height / sideLength;
+columns = canvas.width / sideLength;
+
+
+// snake variables
+var position;
+
+// food
+var foodX, foodY;
+
+// commands
+var key; //leftPressed, upPressed, rightPressed, downPressed;
+
+
+// EVENT LISTENERS
+addEventListener("keydown", function(event) {
+
+    if (event.keyCode === 37 && key !== "right") key = "left";
+    if (event.keyCode === 38 && key !== "down") key = "up";
+    if (event.keyCode === 39 && key !== "left") key = "right";
+    if (event.keyCode === 40 && key !== "up") key = "down";
+})
+
+function init() {
+    lives = 1;
+    position = [[2, 2]]
+    key = null;
+
+    generateFood();
+}
+
+function generateFood() {
+    foodX = Math.floor(Math.random() * columns);
+    foodY = Math.floor(Math.random() * rows);
+}
+
+function moveSnake() {
+    var pos = position[position.length - 1]
+
+    if (key === "left")     position.push([pos[0] - 1, pos[1]]);
+    if (key === "up")       position.push([pos[0], pos[1] - 1]);
+    if (key === "right")    position.push([pos[0] + 1, pos[1]]);
+    if (key === "down")     position.push([pos[0], pos[1] + 1]);
+
+    if (position.length > 1) position.shift();
+}
+
+function drawSnake() {
+    ctx.beginPath();
+    for (var i = 0; i < position.length; i++) {
+        ctx.rect(position[i][0]*sideLength, position[i][1]*sideLength, sideLength, sideLength);
+    }
+    ctx.fillStyle = "#fff";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawFood() {
+    ctx.beginPath();
+    ctx.rect(foodX*sideLength, foodY*sideLength, sideLength, sideLength);
+    ctx.fillStyle = "#f09";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    setTimeout( function() {
+        
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        drawFood();
+        moveSnake();
+        drawSnake();
+        
+
+        var pos = position[position.length-1]
+        if (pos[0] === foodX && pos[1] === foodY) {
+            score++;
+            position.unshift(position[0]);
+            
+            generateFood();
+        } else {
+            for (var i = 0; i < position.length-1; i++) {
+                //if (pos === position[i]) alert("shucks");
+                if (pos[0] === position[i][0] && pos[1] === position[i][1]) {
+                    alert("shucks");
+                    init();
+                }
+            }
+            if (pos[0] < 0 || pos[0] > columns - 1 || pos[1] < 0 || pos[1] > rows - 1) {
+                alert("shucks");
+                init();
+            }
+        }
+
+        requestAnimationFrame(draw);
+    }, 40);
+}
+
+init();
+draw();
